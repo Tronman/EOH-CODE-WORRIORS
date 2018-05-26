f4075f73a9e29194c9d62a2740c256aec14457c2
Tronman <u15200796@tuks.co.za>
2018-05-26 06:03:06 +0200

    default route

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/visuals.html'));
+});
 
 app.listen(3000, () => console.log('Webhook server is listening, port 3000'));
